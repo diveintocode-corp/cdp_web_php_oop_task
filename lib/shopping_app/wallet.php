8fd326e44895de5d30b303974458544d957fdb5b
tatsuya matsuhashi <maa0917@gmail.com>
2022-01-06 22:49:15 +0900

    Wallet クラス フォーマット

--- lib/shopping_app/wallet.php
+++ lib/shopping_app/wallet.php
@@ -1,4 +1,7 @@
 <?php
+
+declare(strict_types=1);
+
 class Wallet
 {
     // attr_reader :balance
@@ -11,13 +14,13 @@ class Wallet
 
     public function deposit($amount)
     {
-        $this->balance +=(int) $amount;
+        $this->balance += (int)$amount;
     }
 
     public function withdraw($amount)
     {
         // return unless @balance >= amount
-        if ($this->balance <$amount) {
+        if ($this->balance < $amount) {
             return;
         }
 
