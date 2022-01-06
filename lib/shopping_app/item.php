fa3f81ec37136a87ffa44c77cd53817b47675b63
tatsuya matsuhashi <maa0917@gmail.com>
2022-01-07 00:17:56 +0900

    Item クラス フォーマット

--- lib/shopping_app/item.php
+++ lib/shopping_app/item.php
@@ -1,32 +1,38 @@
 <?php
-class Item{
-  // attr_reader :name, :price
 
-  // @@instances = []
-  // public $this->instances = array(0);
-  public static $instances = array();
+declare(strict_types=1);
 
-  function __construct($name, $price, $owner=nil)
-  {
-      $this->name = $name;
-      $this->price = $price;
-      $this->owner = $owner;
-      // $instances = array();
-      # Itemインスタンスの生成時、そのItemインスタンス(self)は、@@insntancesというクラス変数に格納されます。
-      // $this->instances << self
-      array_push(self::$instances, $this);
-  }
+class Item
+{
+    // attr_reader :name, :price
 
-  function label(){
-    // { name: name, price: price }
-    $name ="name";
-    $price = "price";
-  }
+    // @@instances = []
+    // public $this->instances = array(0);
+    public static $instances = array();
 
-  function all(){
-      #　@@instancesを返します ==> Item.allでこれまでに生成されたItemインスタンスを全て返すということです。
-      // @@instances
-      return self::$instances;
-  }
+    function __construct($name, $price, $owner = nil)
+    {
+        $this->name = $name;
+        $this->price = $price;
+        $this->owner = $owner;
+        // $instances = array();
+        # Itemインスタンスの生成時、そのItemインスタンス(self)は、@@insntancesというクラス変数に格納されます。
+        // $this->instances << self
+        array_push(self::$instances, $this);
+    }
+
+    function label()
+    {
+        // { name: name, price: price }
+        $name = "name";
+        $price = "price";
+    }
+
+    function all()
+    {
+        #　@@instancesを返します ==> Item.allでこれまでに生成されたItemインスタンスを全て返すということです。
+        // @@instances
+        return self::$instances;
+    }
 
 }
